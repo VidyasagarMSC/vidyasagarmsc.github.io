63e80845d07f6d8c56859369435de7a374d2392a
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2018-07-10 19:21:07 +0530

    Updated Instagram link

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -81,7 +81,7 @@ export default {
         { title: 'Profile', icon: 'linkedin', link: 'https://www.linkedin.com/in/vidyasagarmsc' },
         { title: 'Facebook', icon: 'facebook', link: 'http://facebook.com/mscvidyasagar' },
         // { title: 'Videos', icon: 'youtube-play', link: 'https://www.youtube.com/user/mscvidyasagar' },
-        { title: 'Photos', icon: 'instagram', link: 'http://instagram.com/vidyasagarmsc/' },
+        { title: 'Photos', icon: 'instagram', link: 'http://instagram.com/vmacfotography/' },
         { title: 'Slides', icon: 'slideshare', link: 'https://www.slideshare.net/vidyasagarMachupalli' }
       ],
       nav: {
