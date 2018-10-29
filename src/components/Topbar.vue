83b9fdfc1ec26d6d45a1680348919293ee02f7d2
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2018-10-29 11:46:20 +0530

    Remove Home button

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -69,7 +69,7 @@ export default {
       drawer: false,
       mini: false,
       locals: [
-        { title: 'Home', icon: 'home', link: '/' }
+        // { title: 'Home', icon: 'home', link: '/' }
         // { title: 'Engagements', icon: 'user-o', link: '/engagements' },
         // { title: 'Writes', icon: 'pencil', link: '/writes' },
         // { title: 'Talks', icon: 'play', link: '/videos' },
