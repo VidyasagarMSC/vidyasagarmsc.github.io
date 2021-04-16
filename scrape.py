1a18e1b8758c19a1d311d8b2e22980d422c47300
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2021-04-16 19:05:15 +0530

    Updated the python file

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 # TODO: Add concurrency and simplify the replace :)
 from requests import get
 from bs4 import BeautifulSoup
+import os
 
 
 def scrape_data(tag, url, attributes):
@@ -27,6 +28,11 @@ if __name__ == "__main__":
         file_data = file_data.replace("{{ dzone_views }}", dzone_views[-1].text) \
             .replace("{{ medium_followers }}", medium_followers[-1].text.split(" ")[0]) \
             .replace("{{ wordpress_followers }}", wordpress_followers[-1].text.split(" ")[1])
+        if os.path.exists("blog.html"):
+            os.remove("blog.html")
+        else:
+            print("The file does not exist")
+
     with open("blog.html", "w") as output_file:
         print(file_data)
         output_file.write(file_data)
