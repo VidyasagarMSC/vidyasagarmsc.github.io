966ba154371146c5aa11e8845d584933c990fa4c
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2022-01-22 18:24:17 -0600

    Fix the git action

--- scrape.py
+++ scrape.py
@@ -23,16 +23,17 @@ if __name__ == "__main__":
     dzone_views = scrape_data(
         "p", "https://dzone.com/users/2567192/vidyasagarmsc.html", {'class': 'user-score'})
     # print(dzone_views[-1].text)
-    medium_followers = scrape_data(
-        "a", "https://vidyasagarmsc.medium.com/", {'href': '/followers?source=user_profile----------------------------------------'})
-    print(medium_followers[-1].text.split(" ")[0])
+    medium_followers_content = scrape_data(
+        "meta", "https://vidyasagarmsc.medium.com/followers", {'name': 'description'})
+    medium_followers = medium_followers_content[0].get("content")
+    #print(medium_followers.split(" ")[0])
     wordpress_followers = scrape_data(
         "div", "https://vmacwrites.wordpress.com/", {'class': 'jetpack-subscribe-count'})
     #print(wordpress_followers[-1].text.split(" ")[1])
     with open("templates/blog_template.html", "r") as input_file:
         file_data = input_file.read()
         file_data = file_data.replace("{{ dzone_views }}", dzone_views[-1].text) \
-            .replace("{{ medium_followers }}", medium_followers[-1].text.split(" ")[0]) \
+            .replace("{{ medium_followers }}", medium_followers.split(" ")[0]) \
             .replace("{{ wordpress_followers }}", wordpress_followers[-1].text.split(" ")[1])
         if os.path.exists("blog.html"):
             os.remove("blog.html")
@@ -40,5 +41,5 @@ if __name__ == "__main__":
             print("The file does not exist")
 
     with open("blog.html", "w") as output_file:
-        print(file_data)
+        #print(file_data)
         output_file.write(file_data)
