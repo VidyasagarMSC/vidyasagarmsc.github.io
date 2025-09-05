d50674a4635381c0af98e760816bb54b9ce5e0d7
Sarath Chandra Vidya Sagar Machupalli <vidyasagar.msc@ibm.com>
2025-09-05 16:23:45 -0500

    Updated stats

--- scrape.py
+++ scrape.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 # Constants
 TODAY = date.today().strftime("%B %d, %Y")
 BLOG_DEFAULTS = {
-    "dzone_views": "500K+",
+    "dzone_views": "510K+",
     "medium_followers": "653",
     "wordpress_followers": "1.1K",
     "last_updated": TODAY,
