a4a21bb4c2555a0bc0b7c87fb689d515f87e677d
Sarath Chandra Vidya Sagar Machupalli <vidyasagar.msc@ibm.com>
2025-10-23 13:46:19 -0500

    Fix for mobile menu not loading

--- js/index.js
+++ js/index.js
@@ -82,11 +82,12 @@ function myFunction() {
   });
 
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function () {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
+  // Note: Mobile navigation is handled by vanilla JavaScript in HTML files
+  // $(".navbar-burger").click(function () {
+  //   // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+  //   $(".navbar-burger").toggleClass("is-active");
+  //   $(".navbar-menu").toggleClass("is-active");
+  // });
 
   $(".toggler").on("click", () => {
     $(".fullscreen").toggleClass("light");
